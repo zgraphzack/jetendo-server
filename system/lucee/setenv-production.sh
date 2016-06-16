cd0e7806113da0a0b3d9cf66f0cee17735e8617c
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2016-06-16 18:20:42 -0400

    minor updates

--- system/lucee/setenv-production.sh
+++ system/lucee/setenv-production.sh
@@ -3,7 +3,7 @@
 # -Xmx<size> set maximum Java heap size
 # -Xss<size> set java thread stack size
 # -XX:MaxPermSize sets the java PermGen size
-CATALINA_OPTS="-server -Dsun.io.useCanonCaches=false -Xms1524m -Xmx3324m -javaagent:lib/lucee-inst.jar  -Djava.library.path=/usr/local/apr/lib -XX:+OptimizeStringConcat -XX:+UseTLAB -XX:+UseBiasedLocking -Xverify:none -XX:+UseThreadPriorities  -XX:+UseFastAccessorMethods -XX:-UseLargePages -XX:+UseCompressedOops";
+CATALINA_OPTS="-server -Dsun.io.useCanonCaches=false -Xms1524m -Xmx3324m -javaagent:/var/jetendo-server/lucee/tomcat/lucee-server/context/lucee-external-agent.jar  -Djava.library.path=/usr/local/apr/lib -XX:+OptimizeStringConcat -XX:+UseTLAB -XX:+UseBiasedLocking -Xverify:none -XX:+UseThreadPriorities  -XX:+UseFastAccessorMethods -XX:-UseLargePages -XX:+UseCompressedOops";
 
 # additional JVM arguments can be added to the above line as needed, such as
 # custom Garbage Collection arguments.
